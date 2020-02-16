bf133eeae712916b1c474b8bdc893e38e51a8d9c
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2020-02-16 14:22:26 -0500

    Se agrega backup de la base de datos

---
+++ conexion/Conexion.php
@@ -0,0 +1,12 @@
+<?php 
+
+    class Conexion{
+
+        public function getConexion(){
+            $conexion = new mysqli("localhost", "root", "111", "bdcolegio");
+            $conexion->query("SET NAMES 'utf8'");
+            return $conexion;
+        }
+    }
+
+?>
